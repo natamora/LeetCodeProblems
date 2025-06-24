3b9d43e843d77bfebe5d45eec3363c1ec6ca97a0
Natamora <natamora96@gmail.com>
2025-06-24 18:19:40 +0200

    init: Add workflow to run .NET tests and example solution + dotnet-tests.yml to run tests after push + example: Longest Substring Without Repeating Characters solution

---
+++ csharp/LeetCodeProblems.Tests/Algorithms/Problem0003_LongestSubstringWithoutRepeatingCharacters/SolutionTests.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeProblems.Algorithms.Problem0003_LongestSubstringWithoutRepeatingCharacters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Tests.Algorithms.Problem0003_LongestSubstringWithoutRepeatingCharacters
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void LongestSubstringWithoutRepeatingCharactersTest1()
+        {
+            var solution = new Solution();
+            var result = solution.LengthOfLongestSubstring("abcabcbb");
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void LongestSubstringWithoutRepeatingCharactersTest2()
+        {
+            var solution = new Solution();
+            var result = solution.LengthOfLongestSubstring("bbbbb");
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void LongestSubstringWithoutRepeatingCharactersTest3()
+        {
+            var solution = new Solution();
+            var result = solution.LengthOfLongestSubstring("pwwkew");
+            Assert.Equal(3, result);
+        }
+    }
+}
---
+++ csharp/LeetCodeProblems/Algorithms/Problem0003_LongestSubstringWithoutRepeatingCharacters/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Algorithms.Problem0003_LongestSubstringWithoutRepeatingCharacters
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var dict = new Dictionary<char, int>();  // letter, latest index
+            int maxLen = 0;
+            // iter with right to look for the longest substring
+            for (int left = 0, right = 0; right < s.Length; right++)
+            {
+                char c = s[right];
+                // if we already found this letter before we need to move left pointer to the position of last occurence
+                if (dict.ContainsKey(c))
+                {
+                    left = Math.Max(dict[c] + 1, left); //move left to the next char after the one that repeat 
+                                                        //(or stay with left index if we already skipped this position)
+                }
+                // update or add letter to dict
+                dict[c] = right;
+                maxLen = maxLen < (right - left + 1) ? (right - left + 1) : maxLen;
+            }
+            return maxLen;
+        }
+    }
+}
